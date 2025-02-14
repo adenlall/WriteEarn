d0b5f4c78790780bf7b77b0027c3f69271bcc422
adenlall <adenlall@outlook.sa>
2025-02-14 15:08:36 +0100

    💾 Better DatabaseSeeder.php

--- back/database/seeders/DatabaseSeeder.php
+++ back/database/seeders/DatabaseSeeder.php
@@ -3,9 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Blog;
-use App\Models\Offer;
-use App\Models\Payment;
-use App\Models\Post;
 use App\Models\ReaderSubscription;
 use App\Models\SubscriptionPlan;
 use App\Models\User;
@@ -19,7 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
         User::create([
             'role' => 'admin',
             'name' => 'adenlall',
@@ -27,49 +23,14 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make('0'),
             'username' => 'adenlall',
         ]);
-        $publishers = User::factory()->publisher()->count(10)->create();
-        $readers = User::factory()->reader()->count(10)->create();
 
-        User::factory()->admin()->count(5)->create();
+        Blog::factory()
+            ->hasPosts()
+            ->has(User::factory()->publisher())
+            ->has(SubscriptionPlan::factory()->count(3))
+            ->count(10)
+            ->create();
 
-        $publishers->each(function ($publisher) use ($readers) {
-
-            $blogs = Blog::factory()->count(2)->create([
-                'user_id' => $publisher->id,
-            ]);
-
-            $blogs->each(function ($blog) use ($readers) {
-
-                Post::factory()->count(5)->create([
-                    'blog_id' => $blog->id,
-                ]);
-
-                $plans = SubscriptionPlan::factory()->count(2)->create([
-                    'blog_id' => $blog->id,
-                ]);
-
-                $plans->each(function ($plan) use ($readers) {
-
-                    if (rand(0, 1)) {
-                        Offer::factory()->create([
-                            'subscription_plan_id' => $plan->id,
-                        ]);
-                    }
-
-                    for ($i = 0; $i < 5; $i++) {
-                        $reader = $readers->random();
-                        $subscription = ReaderSubscription::factory()->create([
-                            'user_id' => $reader->id,
-                            'subscription_plan_id' => $plan->id,
-                        ]);
-                        Payment::factory()->create([
-                            'reader_subscription_id' => $subscription->id,
-                        ]);
-                    }
-
-                });
-            });
-        });
-
+        ReaderSubscription::factory()->count(50)->create();
     }
 }
